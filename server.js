9272b8eb8695712dfb8d62cd375524ba95579a7d
Zachary Kitto <zmkitto11@gmail.com>
2021-08-21 22:21:17 -0400

    Added Procfile

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-app.use(express.static(__dirname + '/dist/my-app'));
-
-app.get('/*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/dictionary/index.html'));
-});
-
-
-app.listen(process.env.PORT || 8080);
+app.use(requireHTTPS);
+app.use(express.static('./dist/my-app'));
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/my-app/'}
+  ),
+  );
+  app.listen(process.env.PORT || 8080);
