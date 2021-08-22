bc7fe0236e7829449c33fcbf9abb3c74562eb679
Zachary Kitto <zmkitto11@gmail.com>
2021-08-21 21:52:20 -0400

    Changes to server.js

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}const express = require('express');
+const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(requireHTTPS);
-app.use(express.static('./dist/<name-on-package.json>'));
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
-  );
-  });
-  app.listen(process.env.PORT || 8080);
+
+app.use(express.static(__dirname + '/dist/my-app'));
+
+app.get('/*', (req,res,next) => {
+    res.sendFile(path.join(__dirname + '/dist/dictionary/index.html'));
+});
+
+
+app.listen(process.env.PORT || 8080);
